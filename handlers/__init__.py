661582727704004e45948ffabf6d0878a122ec2f
linguini1 <matteo.golin@gmail.com>
2022-04-05 14:34:54 -0400

    User input is no longer required on screenless devices.

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -332,14 +332,17 @@ class BasicsHandler(EventHandler):
         )
 
         # Return response
-        webhook_response = r.WebhookResponse(
-            session_id=self.session_id,
-            simples=[intro],
-            list_=list_response
-        )
-
         if "RICH_RESPONSE" in self.parser.device.capabilities:
-            webhook_response.simples.append(headline_response)
+            webhook_response = r.WebhookResponse(
+                session_id=self.session_id,
+                simples=[intro],
+                list_=list_response
+            )
+        else:
+            webhook_response = r.WebhookResponse(
+                session_id=self.session_id,
+                simples=[intro, headline_response],
+            )
 
         return webhook_response
 
